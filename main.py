c9af6cf3d60ee32f8bfa4c5952ec851131515bcf
marc <marcvanduyn1@gmail.com>
2022-04-24 21:19:41 +0200

    feat(VERSION): bump version

--- main.py
+++
@@ -1,7 +0,0 @@
-from eltyer import Client
-
-if __name__ == "__main__":
-    client = Client()
-    client.config.API_KEY = "cbnu5EUlzF3empnASvYvQzTwSsiQTiAXiKIvDvT7ZLM3wXhYhaG2vTAlKFL4tNYn"
-    client.start()
-    print(client.get_positions(json=True))
